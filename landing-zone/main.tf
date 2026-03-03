9403b8a808233ca5cd2b157a95c709b4e628d054
CloudNexus <abhighnagiri@gmail.com>
2026-03-03 11:36:39 +0530

    Update main.tf

--- landing-zone/main.tf
+++ landing-zone/main.tf
@@ -15,3 +15,19 @@ module "budget" {
   source          = "./modules/budget-alerts"
   billing_account = var.billing_account
 }
+module "iam_baseline" {
+  source      = "./modules/iam-baseline"
+  project_id  = module.org_structure.network_project_id
+  environment = var.environment
+}
+
+module "logging" {
+  source          = "./modules/logging-centralization"
+  org_id          = var.org_id
+  billing_account = var.billing_account
+}
+
+module "org_policies" {
+  source = "./modules/policy-enforcement"
+  org_id = var.org_id
+}
